93030db0a6ca55211cdd5eb0d6cde8028605251e
OKuhle <okuhlexesibe0@gmail.com>
2026-02-13 21:17:29 +0000

    One is better than nothing i guess

---
+++ leetcode/easy/go/TypeOfTriangle.go
@@ -0,0 +1,22 @@
+package go_test
+
+import "sort"
+func triangleType(nums []int) string {
+
+	sort.Ints(nums)
+	
+    if nums[0] + nums[1] <= nums[2] {
+        return "none"
+    }
+
+    if nums[0] == nums[2] {
+        return "equilateral"
+    }
+
+    if nums[0] == nums[1] || nums[1] == nums[2] {
+        return "isosceles"
+    }
+
+    return "scalene"
+
+}
