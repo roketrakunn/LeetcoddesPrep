80e82b481a04b22ca18c6506bac54bf96149f0c6
Roket <okuhlexesibe0@gmail.com>
2026-03-01 22:22:12 +0000

    Convert to parity and sort problem

---
+++ leetcode/easy/go/toParity.go
@@ -0,0 +1,16 @@
+package go_test
+
+import "slices"
+
+func transformArray(nums []int) []int {
+    
+	for i ,val := range nums { 
+		if val % 2 != 0 { 
+			nums[i] = 1
+		}else { 
+			nums[i] = 0
+		}
+	}
+	slices.Sort(nums)
+	return nums
+}
