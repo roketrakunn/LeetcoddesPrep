4d2f3cff13b7ea13eca0db823f918a0b352ad2ba
Roket <okuhlexesibe0@gmail.com>
2026-03-24 23:48:11 +0200

    Solution : Problem 2830

--- leetcode/medium/src/main.rs
+++ leetcode/medium/src/main.rs
@@ -1,7 +1,7 @@
 mod sub_seqs;
 mod maxi_string_pairs;
 mod k_partitions;
-
+mod maxi_gold_profit;
 
 fn main() {
     println!("Hello, world!");
---
+++ leetcode/medium/src/maxi_gold_profit.rs
@@ -0,0 +1,29 @@
+struct Solution; 
+
+use std::{collections::HashMap, usize};
+
+impl Solution {
+    pub fn maximize_the_profit(n: i32, offers: Vec<Vec<i32>>) -> i32 {
+        let mut  group_by_ends = HashMap::new();
+
+        for offer in &offers {
+            group_by_ends.entry(offer[1])
+                .or_insert(vec![])
+                .push(offer);
+        }
+
+        let mut dp = vec![0i32; (n+1) as usize]; 
+        for i in 1..=n { 
+            dp[i as usize] = dp[(i -1) as usize];
+
+            if let Some(ending_offers) = group_by_ends.get(&(i-1)) { 
+                for offer in ending_offers { 
+                    dp[i as usize] = dp[i as usize].max(dp[offer[0] as usize ] 
+                        + offer[2]);
+                }
+            }
+
+        }
+       dp[n as usize] 
+    }
+}
